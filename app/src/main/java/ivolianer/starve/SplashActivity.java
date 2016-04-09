b0c2ea003da2a4406709944f026c604019ba3660
renjiajian <renjiajian@withub.net.cn>
2016-04-09 18:14:24 +0800

    SplashActivity 解决了白屏问题

--- app/src/main/java/ivolianer/starve/SplashActivity.java
+++ app/src/main/java/ivolianer/starve/SplashActivity.java
@@ -1,47 +1,7 @@
 package ivolianer.starve;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 
-// TODO setBg to window to improve splash's performance
 public class SplashActivity extends AppCompatActivity {
-
-    @Bind(R.id.imageView)
-    ImageView imageView;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        ButterKnife.bind(this);
-
-
-        Picasso.with(this).load(R.drawable.big).into(imageView);
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Bitmap bitmap =((BitmapDrawable) imageView.getDrawable()).getBitmap();
-//                Log.e("result","");
-            }
-        });
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        }, 1000);
-    }
-
 }
